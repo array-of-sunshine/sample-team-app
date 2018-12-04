db8bf659e5361c72dbdc6e35c9a12899a3c5cf4e
ShaundaK <sdking1913@gmail.com>
2018-12-03 18:47:27 -0600

    show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/pages' => 'pages#index'
+    post '/pages' => 'pages#create'
     get '/pages' => 'pages#show'
+
+
   end
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
